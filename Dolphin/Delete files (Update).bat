9599a39bb2c701429f74f611401af1b330384b88
Bird <39280243+jlambert360@users.noreply.github.com>
2022-01-23 06:18:07 -0500

    Build template files

---
+++ Dolphin/Delete files (Update).bat
@@ -0,0 +1,24 @@
+rmdir .\User\Cache /s /q
+rmdir .\User\Dump /s /q
+rmdir .\User\GC /s /q
+rmdir .\User\Logs /s /q
+rmdir .\User\Maps /s /q
+rmdir .\User\ScreenShots /s /q
+rmdir .\User\Shaders /s /q
+rmdir .\User\StateSaves /s /q
+rmdir .\User\Themes /s /q
+rmdir .\User\Wii\import /s /q
+rmdir .\User\Wii\meta /s /q
+rmdir .\User\Wii\shared1 /s /q
+rmdir .\User\Wii\ticket /s /q
+rmdir .\User\Wii\title /s /q
+rmdir .\User\Wii\tmp /s /q
+rmdir .\User\Wii\wfs /s /q
+del .\vcruntime140_1.dll /Q
+del .\User\Config\Dolphin.ini /Q
+del .\User\Config\GCKeyNew.ini /Q
+del .\User\Config\GCPadNew.ini /Q
+del .\User\Config\GFX.ini /Q
+del .\User\Config\Hotkeys.ini /Q
+del .\User\Config\Logger.ini /Q
+del .\User\Config\WiimoteNew.ini /Q
